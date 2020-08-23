8ba1f7f876e6f5d6807e6b66cf34816b3bea117a
Suhayb Alghutaymil <s@sgh.sa>
2020-08-23 20:24:33 +0300

    Add Cheese Detail.

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from everycheese.cheeses.models import Cheese
 
 
 class CheeseListView(ListView):
+    model = Cheese
+
+
+class CheeseDetailView(DetailView):
     model = Cheese
